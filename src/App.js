a5b6aebf6d4c76b9f285bbd15bcb533ddc7ad28b
박재현 <pjh@yisistory.com>
2022-08-08 21:19:51 +0900

    modified
    첫번째 수정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+	state = {
+		todoData : [
+			{
+				id: "1",
+				title: "공부하기",
+				completed: true
+			},
+			{
+				id: "2",
+				title: "청소하기",
+				completed: false
+			}
+		]
+	}
+
+	btnStyle = {
+		color: "#fff",
+		border: "none",
+		padding: "5px 9px",
+		borderRadius: "50%",
+		cursor: "pointer",
+		float: "right"
+	}
+
+	// 동적 스타일을 위해 함수로 구현
+	getStyle = () => {
+		return {
+			padding: "10px",
+			borderBottom: "1px #ccc dotted",
+			textDecoration: "none"
+		}
+	}	
+
+	handleClick = (id) => {
+		let newTodoData = this.state.todoData.filter(data => data.id !== id)
+		console.log('newTodoData', newTodoData);
+
+		this.setState({todoData: newTodoData});
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<div className="todoBlock">
+					<div className="title">
+						<h1>할일 목록</h1>
+					</div>
+
+					{this.state.todoData.map((data) => (
+						// 아래의 key가 있어야 다시 rendering할때 기존 부분은 재 렌더링 되지 않는다.
+						<div style={this.getStyle()} key={data.id}>
+							<input type="checkbox" defaultChecked={false} />
+							{data.title}
+							<button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
+						</div>	
+					))}
+
+				</div>
+			</div>
+		)
+	}
+}
